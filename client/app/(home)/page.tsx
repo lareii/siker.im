d0799985a7049520e885e8f55c58191ddd1429c6
lareii <lareii@proton.me>
2025-07-09 17:12:12 +0300

    fix(client): remove loading.tsx file for improve loading

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
       <Navbar />
