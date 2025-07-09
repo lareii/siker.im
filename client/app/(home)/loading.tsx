fea1034d193914dd552f2c8c0fb3e6897b1e3355
lareii <lareii@proton.me>
2025-07-09 17:03:19 +0300

    feat(client): add loading page with transistion

---
+++ client/app/(home)/loading.tsx
@@ -0,0 +1,15 @@
+import { IconInnerShadowBottomLeftFilled } from '@tabler/icons-react';
+
+export default function TestMotion() {
+  return (
+    <div className="flex flex-col gap-y-2 items-center justify-center h-screen">
+      <div className="text-4xl font-bold text-primary">
+        siker.im/
+      </div>
+      <div className='flex items-center gap-x-2 text-sm'>
+        <IconInnerShadowBottomLeftFilled className='animate-spin w-5 h-5 text-primary' />
+        yükleniyor...
+      </div>
+    </div>
+  );
+}
--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,13 +1,22 @@
+'use client';
+
 import Navbar from '@/components/app/navbar';
 import Footer from '@/components/app/home/footer';
 import MainContent from '@/components/app/home/main-content';
 
+import { motion } from 'framer-motion';
+
 export default function Home() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <Navbar />
       <MainContent />
       <Footer />
-    </>
+    </motion.div>
   );
 }
